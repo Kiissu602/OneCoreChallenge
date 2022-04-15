b19b9434a7e8f9ad20603e9c06617d8a128f6bf8
Phumin <poolmean602@gmail.com>
2022-04-15 17:49:08 +0700

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://products.onecore.website',
+  refreshToken:
+    'JEJIJIf3FjwbcziU5abODCXGfjIzJXrASxXDfWMkBQbzcO4iWL_M5902Slk789yYGyWKmPK8GdXP3PLAfSR5ke4iSB75vuWr68TerQQhausl8ck8e53IPGHsZbRBta0sZ58_WOYBOaRXkk5HiGiOQHRGG2vjdBOPACPMtX9A142F98WCKqNRJdoru16KyxRC28s66j2ZZ5aGRORz5XEcWRVPP7s0pZiDYsz01vVxC1RVuTB1fX8qCaO5vTpPoh5p',
 };
